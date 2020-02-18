89dc5733c4da86bf197d8f8b3431456ee2445298
erikrios <erikriosetiawan49@gmail.com>
2020-02-18 23:17:10 +0700

    Add data key for intent

--- app/src/main/java/com/erikriosetiawan/loginanddashboardpage/DashboardActivity.kt
+++ app/src/main/java/com/erikriosetiawan/loginanddashboardpage/DashboardActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class DashboardActivity : AppCompatActivity() {
 
+    companion object {
+        const val DATA_EMAIL_KEY = "data_email_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
