dd527b37fa708833f68ea00a55db65f1f52303ef
erikrios <erikriosetiawan49@gmail.com>
2020-02-18 23:25:57 +0700

    Add method to get data from intent

--- app/src/main/java/com/erikriosetiawan/loginanddashboardpage/DashboardActivity.kt
+++ app/src/main/java/com/erikriosetiawan/loginanddashboardpage/DashboardActivity.kt
@@ -2,8 +2,13 @@ package com.erikriosetiawan.loginanddashboardpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.erikriosetiawan.loginanddashboardpage.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    // Declare the data binding
+    private lateinit var binding: ActivityDashboardBinding
 
     companion object {
         const val DATA_EMAIL_KEY = "data_email_key"
@@ -11,6 +16,14 @@ class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        // Assign the data binding with content view
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
+
+        // Set welcome text
+        binding.tvWelcome.text = resources.getString(R.string.welcome_text, getDataIntent())
     }
+
+    // Get data from intent
+    private fun getDataIntent() = intent?.getStringExtra(DATA_EMAIL_KEY)
 }
