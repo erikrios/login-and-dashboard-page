d7b1aaf39d7f8760d23ae528db76a1ac2f8445dd
erikrios <erikriosetiawan49@gmail.com>
2020-02-18 23:20:12 +0700

    Integrated the data binding and passing a data between intent

--- app/src/main/java/com/erikriosetiawan/loginanddashboardpage/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/loginanddashboardpage/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.erikriosetiawan.loginanddashboardpage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.erikriosetiawan.loginanddashboardpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // Declare the data binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Assign the data binding with content view
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    // Passing data using intent between activities
+    private fun sendEmailData() {
+
+        // Validate the login form
+        var isEmptyEmail = false
+        var isEmptyPassword = false
+
+        if (binding.tvEmailAddress.text.toString().isEmpty()) {
+            isEmptyEmail = true
+            binding.tvEmailAddress.error = getString(R.string.empty_email_address_error_message)
+        }
+
+        if (binding.tvPassword.text.toString().isEmpty()) {
+            isEmptyPassword = true
+            binding.tvPassword.error = getString(R.string.empty_password_error_message)
+        }
+
+        if (!isEmptyEmail && !isEmptyPassword) {
+            // Passing data using explicit intent
+            val intent = Intent(this, DashboardActivity::class.java)
+            intent.putExtra(
+                DashboardActivity.DATA_EMAIL_KEY,
+                binding.tvEmailAddress.text.toString()
+            )
+            startActivity(intent)
+        }
     }
 }
